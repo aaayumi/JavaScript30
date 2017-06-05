c91268348daa2711ec090dcdc53f6a52ad2ac460
Ayumi Saito <ayumisaito27@gmail.com>
2017-06-05 09:36:04 +0200

    update

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,7 @@
+### Image slider 
+
+## June 2nd 
+I made foundation with jQuery. I will add styling and more functionality tomorrow.
+
+Reference :
+[Simplest jQuery Slideshow](https://snook.ca/archives/javascript/simplest-jquery-slideshow)
